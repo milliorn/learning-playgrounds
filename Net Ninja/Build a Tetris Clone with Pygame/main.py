b06ccb1c30330a13cd072c046f95e14079ff72e1
milliorn <scottmilliorn@gmai.com>
2023-09-20 12:23:47 -0700

    comments

--- Net Ninja/Build a Tetris Clone with Pygame/main.py
+++ Net Ninja/Build a Tetris Clone with Pygame/main.py
@@ -1,26 +1,32 @@
+# Importing necessary settings and modules
 from settings import *
 from sys import exit
+
+# Main class definition
 
 
 class Main:
     def __init__(self):
-        pygame.init()
-        pygame.display.set_caption('Tetris')
-        self.clock = pygame.time.Clock()
-        self.display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.init()  # Initialize Pygame
+        pygame.display.set_caption('Tetris')  # Set the window caption
+        self.clock = pygame.time.Clock()  # Create a Pygame clock instance
+        self.display = pygame.display.set_mode(
+            (WINDOW_WIDTH, WINDOW_HEIGHT))  # Set the display dimensions
 
     def run(self):
+        # Main game loop
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit()
+                    pygame.quit()  # Quit Pygame
+                    exit()  # Exit the program
 
-            self.display.fill(GRAY)
-            pygame.display.update()
-            self.clock.tick(60)
+            self.display.fill(GRAY)  # Fill the display with a gray background
+            pygame.display.update()  # Update the display
+            self.clock.tick(60)  # Cap the frame rate to 60 frames per second
 
 
+# Entry point of the program
 if __name__ == '__main__':
-    main = Main()
-    main.run()
+    main = Main()  # Create an instance of the Main class
+    main.run()  # Run the game loop
