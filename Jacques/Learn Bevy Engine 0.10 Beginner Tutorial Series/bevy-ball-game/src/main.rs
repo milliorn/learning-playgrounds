e5e171bd7070783053e209d6c6aafb1221deaf86
milliorn <scottmilliorn@gmai.com>
2023-10-01 14:41:28 -0700

    ep7

--- Jacques/Learn Bevy Engine 0.10 Beginner Tutorial Series/bevy-ball-game/src/main.rs
+++ Jacques/Learn Bevy Engine 0.10 Beginner Tutorial Series/bevy-ball-game/src/main.rs
@@ -1,3 +1,31 @@
+pub mod events;
+mod systems;
+
+pub mod enemy;
+mod player;
+pub mod score;
+pub mod star;
+
+use events::*;
+use systems::*;
+
+use enemy::EnemyPlugin;
+use player::PlayerPlugin;
+use score::ScorePlugin;
+use star::StarPlugin;
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_event::<GameOver>()
+        .add_plugin(EnemyPlugin)
+        .add_plugin(PlayerPlugin)
+        .add_plugin(ScorePlugin)
+        .add_plugin(StarPlugin)
+        .add_startup_system(spawn_camera)
+        .add_system(exit_game)
+        .add_system(handle_game_over)
+        .run();
 }
