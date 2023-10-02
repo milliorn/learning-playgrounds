0d39c68a0880eaf482065cfa9e7f42cc1965f687
milliorn <scottmilliorn@gmai.com>
2023-10-02 11:11:31 -0700

    EP8 - Explicit System Ordering Bevy System Sets

--- Jacques/Learn Bevy Engine 0.10 Beginner Tutorial Series/bevy-ball-game/src/player/mod.rs
+++ Jacques/Learn Bevy Engine 0.10 Beginner Tutorial Series/bevy-ball-game/src/player/mod.rs
@@ -10,13 +10,20 @@ use systems::*;
 // Define a struct for the plugin
 pub struct PlayerPlugin;
 
+#[derive(Debug, Hash, PartialEq, Eq, Clone, SystemSet)]
+pub struct MovementSystemSet;
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, SystemSet)]
+pub struct ConfinementSystem;
+
 // Implement the Plugin trait for the plugin
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         // Add startup and update systems to the app
-        app.add_startup_system(spawn_player)
-            .add_system(player_movement)
-            .add_system(confine_player_movement)
+        app.configure_set(MovementSystemSet.before(ConfinementSystem))
+            .add_startup_system(spawn_player)
+            .add_system(player_movement.in_set(MovementSystemSet))
+            .add_system(confine_player_movement.in_set(ConfinementSystem))
             .add_system(enemy_hit_player)
             .add_system(player_hit_star);
     }
